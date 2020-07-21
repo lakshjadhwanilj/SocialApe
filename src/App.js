62eca3beedcff1c2b4511503b4746a8040602b6a
lakshjadhwanilj <lakshjadhwani.lj@gmail.com>
2020-07-21 18:16:11 +0530

    Added Dark Mode toggle using material ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+// MATERIAL UI
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { Switch } from '@material-ui/core';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 // CSS
 import './App.css';
 
 function App() {
+
+  const [darkMode, setDarkMode] = useState(false);
+
+  const theme = createMuiTheme({
+    palette: {
+      type: darkMode ? 'dark' : 'light',
+    },
+  });
+
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Switch checked={darkMode} color='primary' onChange={() => setDarkMode(!darkMode)} />
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    </ThemeProvider>
   );
 }
 
